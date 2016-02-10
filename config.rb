31370b5c59b00f9a2ea1f9a6fe8258234cf44253
Harry Bridge <harrybridge01@gmail.com>
2016-02-10 22:06:05 +0000

    Added compass config

--- config.rb
+++ config.rb
@@ -1,12 +1,18 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'materialize-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "static/css"
+http_stylesheets_path = "css"
+sass_dir = "src/sass"
+images_dir = "static/imgs"
+http_images_path = "imgs"
+javascripts_dir = "static/js"
+http_javascripts_path = "js"
+fonts_dir = "static/fonts"
+http_fonts_path = "../fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed

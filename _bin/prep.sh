7852a20cd97fcdf228afe276139fbaefa34d7aa8
Harry Bridge <harrybridge01@gmail.com>
2016-03-29 23:21:43 +0100

    move files properly

--- _bin/prep.sh
+++ _bin/prep.sh
@@ -3,5 +3,5 @@
 # If staging setup CNAME and SEO block
 if [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH = "staging" ]]
   then
-  mv CNAME.staging CNAME
+  mv static/CNAME.staging static/CNAME
 fi

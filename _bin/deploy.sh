c5885579334f8ed305bd68ebe40efe0a97ab59bd
Harry Bridge <harry@harrybridge.co.uk>
2016-12-28 19:25:12 +0000

    Some deploy changes

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Only deploy if not PR
-if [[ $TRAVIS_PULL_REQUEST == "false"  && ($TRAVIS_BRANCH = "master" || $TRAVIS_BRANCH = "staging") ]]
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]
   then
   # cleanup
   rm -rf gh-pages
@@ -14,29 +14,14 @@ if [[ $TRAVIS_PULL_REQUEST == "false"  && ($TRAVIS_BRANCH = "master" || $TRAVIS_
   fi
 
   # copy generated HTML site to built branch
-  # cp -R public/* gh-pages
+  cp -R public/* gh-pages
 
   # commit and push generated content to built branch
   # since repository was cloned in write mode with token auth - we can push there
-  # cd gh-pages
-
-  git init
-  git checkout -b gh-pages
-
+  cd gh-pages
   git config user.email "hbbot@harrybridge.co.uk"
   git config user.name "hbbot"
   git add -A .
   git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER for $TRAVIS_COMMIT"
-fi
-
-if [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH = "master" ]]
-  then
-  git remote add origin https://${GH_TOKEN}@github.com/pickleshb/Website.git
-  git push --quiet -f origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
-fi
-
-if [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH = "staging" ]]
-  then
-  git remote add origin https://${GH_TOKEN}@github.com/pickleshb/Website-Staging.git
-  git push --quiet -f origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
+  git push --quiet origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
 fi

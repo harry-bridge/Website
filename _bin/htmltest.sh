88fad1bb094046fc66435e14fc64aa28e7b77e2b
Harry Bridge <harry@harrybridge.co.uk>
2017-01-14 22:04:03 +0000

    Working htmltest script

--- _bin/htmltest.sh
+++ _bin/htmltest.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-mkdir ~/htmltest && cd "$_"
-curl -Lk https://github.com/wjdp/htmltest/releases/download/v0.3.0/htmltest-linux 
-mv ~/htmltest* ~/htmltest && chmod +x "$_"
+curl -Lk https://github.com/wjdp/htmltest/releases/download/v0.3.0/htmltest-linux -o ~/htmltest && chmod +x "$_"
 exit 0

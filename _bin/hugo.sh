3051e26bd436953e1d3c772af4c1c77b40a23d1c
Harry Bridge <harry@harrybridge.co.uk>
2017-01-14 21:39:30 +0000

    Updated download scripts

--- _bin/hugo.sh
+++ _bin/hugo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -Lk https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz -o ~/hugo | tar xz
+mkdir ~/hugo && cd "$_"
+curl -Lk https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz | tar xz
 mv ~/hugo*/hugo* ~/hugo018 && chmod +x "$_"
 exit 0
